aa99871e3478089a101c1b7a26c6b02596c9cf37
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2023-12-19 22:57:44 +0300

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
 const burger = document.querySelector('.burger__button')
 const burgerWrapper = document.querySelector('.burger__wrapper')
-const bg = document.querySelector('.bg');
+const bg = document.querySelector('.bg')
+const body = document.querySelector('body')
 
 burger.addEventListener('click', () => {
 	burgerWrapper.classList.toggle('hidden')
 	bg.classList.toggle('bg-black')
+	body.classList.toggle('overflow__hidden')
+	burger.classList.toggle('cross')
 })
+
+// Якорь
+
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		const goto = anchor.hasAttribute('href')
+			? anchor.getAttribute('href')
+			: 'body'
+		document.querySelector(goto).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+}
