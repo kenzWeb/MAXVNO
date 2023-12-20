3f994884ea99f29a3e2bac0a4bb209c7027712ff
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2023-12-20 04:31:23 +0300

    bug fix

--- js/app.js
+++ js/app.js
@@ -36,3 +36,23 @@ for (let anchor of anchors) {
 		})
 	})
 }
+
+// Анимации
+
+function onEntry(entry) {
+	entry.forEach((change) => {
+		if (change.isIntersecting) {
+			change.target.classList.add('show')
+		}
+	})
+}
+
+let options = {
+	threshold: [0.5],
+}
+let observer = new IntersectionObserver(onEntry, options)
+let elements = document.querySelectorAll('.about__wrapper-content')
+
+for (let elm of elements) {
+	observer.observe(elm)
+}
